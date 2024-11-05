32d4de858aa8e1fe73874967d7bc86bf343e7d29
Leolo Flórez <leoloflrzr@gmail.com>
2024-11-05 15:05:14 +0100

    verification of login working and message error created. Add navbar to register and login form

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,14 +1,30 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../utils/supabaseClient";
-import { redirect } from "next/dist/server/api-utils";
-import { Router } from "next/router";
 import { useRouter } from "next/navigation";
+import Navbar from "../components/Navbar";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const router = useRouter()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('')
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const session = supabase.auth.getSession();
+    setUser(session.user);
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_, session) => {
+      setUser(session?.user || null);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,11 +37,12 @@ const Login = () => {
 
       if (error) {
         console.error("Error: " + error.message);
+        setErrorMessage('Error al iniciar sesión. El usuario no está registrado.')
         return;
       }
 
       alert("¡Sesión iniciada correctamente!");
-      return router.push('/')
+      return router.push("/");
     } catch (err) {
       console.error("Unexpected error", err);
       alert("Ocurrió un error inesperado. Inténtalo nuevamente.");
@@ -33,31 +50,36 @@ const Login = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          className="text-black m-4 rounded flex flex-col p-1"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          className="text-black m-4 p-1 rounded"
-          required
-        />
-        <button
-          type="submit"
-          className=" flex flex-col justify-self-center bg-green-800 p-2 rounded hover:bg-green-700 transition duration-500"
-        >
-          Login
-        </button>
-      </form>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Navbar />
+
+      <main className="flex items-center justify-center">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="text-black m-4 rounded flex flex-col p-1"
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="text-black m-4 p-1 rounded"
+            required
+          />
+          <button
+            type="submit"
+            className=" flex flex-col justify-self-center bg-green-800 p-2 rounded hover:bg-green-700 transition duration-500"
+          >
+            Login
+          </button>
+        </form>
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+      </main>
     </div>
   );
 };
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,14 +1,29 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../utils/supabaseClient";
 import { useRouter } from "next/navigation";
+import Navbar from "../components/Navbar";
 
 const Register = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [user, setUser] = useState(null)
   const router = useRouter();
+
+  useEffect(() => {
+    const session = supabase.auth.getSession()
+    setUser(session.user)
+
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_, session) => {
+      setUser(session?.user || null)
+    })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,10 +37,12 @@ const Register = () => {
       if (error) {
         console.error("Error en el registro de usuario: ", error.message);
         // alert(`Error al registrar el usuario: ${error.message}`)
-        setErrorMessage('Este email ya ha sido registrado. Inicia sesión o utiliza otro email para el registro')
+        setErrorMessage(
+          "Este email ya ha sido registrado. Inicia sesión o utiliza otro email para el registro"
+        );
         return;
       } else {
-        setErrorMessage('')
+        setErrorMessage("");
       }
 
       alert("¡Usuario registrado correctamente!");
@@ -37,37 +54,38 @@ const Register = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen ">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          className="text-black m-4 rounded flex flex-col p-1"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          className="text-black m-4 p-1 rounded"
-          required
-        />
-        <button
-          className="flex flex-col justify-self-center bg-green-800 p-2 rounded hover:bg-green-700 transition duration-500"
-          type="submit"
-        >
-          Register
-        </button>
-
-
-      </form>
-      {errorMessage && <p className="text-red-800">{errorMessage}</p>}
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Navbar user={user}/>
+      <main>
+        <div className="flex items-center justify-center">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              className="text-black m-4 rounded flex flex-col p-1"
+              required
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+              className="text-black m-4 p-1 rounded"
+              required
+            />
+            <button
+              className="flex flex-col justify-self-center bg-green-800 p-2 rounded hover:bg-green-700 transition duration-500"
+              type="submit"
+            >
+              Register
+            </button>
+          </form>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </div>
+      </main>
     </div>
-    
-    
   );
 };
 
